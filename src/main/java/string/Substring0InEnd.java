49a977d4e0da80d5a18695e12594e97645f65d06
qinchao-ds <qinchao-ds@gome.com.cn>
2019-02-20 20:08:37 +0800

    2019-2-20 20:07:45    小数点左侧不清除

--- src/main/java/string/Substring0InEnd.java
+++ src/main/java/string/Substring0InEnd.java
@@ -6,14 +6,19 @@ package string;
 public class Substring0InEnd {
 
     public static void main(String[] args) {
-        System.out.println(SubstringRate("0.00"));
+        System.out.println(SubstringRate("1000.00"));
     }
 
     static String SubstringRate(String rate){
         int lastIndex = 0;
         for(int i = 0; i < rate.length(); i++){
+            //标记最后一个非零数位置
             if(rate.charAt(i) > 48 && rate.charAt(i) < 58){
                 lastIndex = i;
+            }
+            // *0.00000 的情况
+            if(rate.charAt(i) == 46){
+                lastIndex = i - 1;
             }
         }
         return rate.substring(0,lastIndex+1);
